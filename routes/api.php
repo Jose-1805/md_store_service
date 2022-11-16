08a8842b53e738977e5b6c247d1034330aca5d2e
jose-1805 <joseluiscapotemosquera@gmail.com>
2022-11-16 18:54:26 -0500

    Cambio en ruta de descarga de logo

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 Route::prefix('store')->group(function () {
     Route::put('toggle-seller', [StoreController::class, 'toggleSeller']);
-    Route::get('download-logo/{store}', [StoreController::class, 'downloadLogo']);
+    Route::get('{store}/logo', [StoreController::class, 'downloadLogo']);
 });
 Route::apiResource('store', StoreController::class);
